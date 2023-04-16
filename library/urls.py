e42ff31ecc3bc303f51b011743737e700150c647
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-04-16 09:53:30 +0300

    refactor: Attach the BookUploadView to a route

--- library/urls.py
+++ library/urls.py
@@ -5,13 +5,14 @@ import library.api_views as library_api_views
 
 
 api_urlpatterns = [
-    path("authors/search/<str:query>", library_api_views.BookAuthorSearchAPIView.as_view(), name="author-search"),
-    path("book-categories/search/<str:query>", library_api_views.BookCategorySearchAPIView.as_view(),
+    path("authors/search", library_api_views.BookAuthorSearchAPIView.as_view(), name="author-search"),
+    path("book-categories/search", library_api_views.BookCategorySearchAPIView.as_view(),
          name="book-category-search")
 ]
 
 urlpatterns = [
     path("books", library_views.BookListView.as_view(), name="book-list"),
+    path("books/upload", library_views.BookUploadView.as_view(), name="book-upload"),
     path("books/<int:book_id>/update", library_views.BookUpdateView.as_view(), name="book-update"),
     path("books/<int:book_id>/delete", library_views.BookDeleteView.as_view(), name="book-delete"),
     path("books/<int:book_id>/dcma/report", library_views.DMCAViolationReportView.as_view(), name="dmca-report"),
