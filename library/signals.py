7917c1141bb587f6a8fc885522c3cd89ef73bcee
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-04-16 09:49:01 +0300

    fix: Restore uploaded book file cursor to the beginning so that subsequent operations wokr normally

--- library/signals.py
+++ library/signals.py
@@ -11,8 +11,10 @@ from .models import Book
 @receiver(pre_save, sender=Book)
 def attach_book_meta_data(sender, instance, *args, **kwargs):
     with fitz.Document(stream=io.BytesIO(instance.file.read())) as pdf:
+        instance.file.seek(0)
         instance.file_size = len(instance.file.read())
         instance.num_of_pages = pdf.page_count
         cover_image_file = ContentFile(pdf.load_page(0).get_pixmap().tobytes(),
                                        name="{}_cover.png".format(slugify(instance.title)))
         instance.cover_image = cover_image_file
+        instance.file.seek(0)  # restore file cursor
