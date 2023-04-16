09d4aff8af048c2c386156311b7d387ef0ca07ed
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-04-16 09:51:31 +0300

    feat: Add pagination to the BookListView and BookCategoryListView

--- library/views.py
+++ library/views.py
@@ -1,9 +1,13 @@
 from functools import cached_property
 
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.db import transaction
 from django.views.generic import View
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView, SingleObjectMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Book
 from .mixins import BookUploaderRequiredMixin
@@ -14,6 +18,7 @@ class BookListView(ListView):
     model = Book
     template_name = "library/book_list.html"
     context_object_name = "books"
+    paginate_by = settings.MY_MAX_ITEMS_PER_PAGE
 
 
 class BookUpdateView(LoginRequiredMixin, BookUploaderRequiredMixin, UpdateView):
@@ -38,7 +43,7 @@ class DMCAViolationReportView(SingleObjectMixin, View):
     model = Book
 
     def get(self, request, *args, **kwargs):
-        """TODO: Log DRMA notice in database and notify the user that uploaded the book"""
+        """TODO: Log DCMA violation notice in database and notify the user(if any) that uploaded the book"""
         pass
 
 
@@ -46,7 +51,28 @@ class BookCategoryListView(ListView):
     model = Book
     template_name = "library/book_category_list.html"
     context_object_name = "book_categories"
+    paginate_by = settings.MY_MAX_ITEMS_PER_PAGE
 
 
-class UploadBookView(LoginRequiredMixin, CreateView):
+class BookUploadView(SuccessMessageMixin, CreateView):
+    template_name = "library/book_list.html"  # Used incase the form is invalid
+    extra_context = {
+        "books": Book.objects.all()
+    }
+    http_method_names = ["post"]
     form_class = BookUploadForm
+    success_url = reverse_lazy("home")
+    success_message = "Book uploaded successfully"
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # Overwrite the global variable 'book_upload_form' so that template receives the form with the right state
+        # (erroneous form) and not the unbound global form (book_upload_form)
+        return self.render_to_response(self.get_context_data(book_upload_form=form))
+
